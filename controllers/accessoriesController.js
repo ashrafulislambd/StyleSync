e4b88c95471b5e07f57f613d3e3e4e482ab42f9a
Md. Ashraful Islam <imdashraful17@gmail.com>
2026-01-23 14:48:09 +0600

    init commit

---
+++ controllers/accessoriesController.js
@@ -0,0 +1,6 @@
+const Accessories = require('../models/accessoriesModel');
+const cloudinary = require('../config/cloudinary');
+
+exports.addAccessory = async (req, res) => {
+   // write the code here
+};
